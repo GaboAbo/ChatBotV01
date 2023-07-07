19a923203e91fb780955610919010377ae0d7803
GaboAbo <robertgabrielmendozatovar@gmail.com>
2023-07-06 22:12:27 -0400

    Refactor: history model added to models.py

--- App/models.py
+++ App/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class FormModel(models.Model):
     content = models.TextField()
+
+class HistoryModel(models.Model):
+    message = models.TextField()
+    msgtime = models.TimeField()
