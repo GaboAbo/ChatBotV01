456c00f212946ab1a22a6d64075a954db1004e5b
GaboAbo <robertgabrielmendozatovar@gmail.com>
2023-07-06 22:11:56 -0400

    Refactor: signin and login forms added to forms.py

--- App/forms.py
+++ App/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Creating a Form to handle the user input and cleaning the values
 class Form(forms.Form):
@@ -16,3 +18,48 @@ class Form(forms.Form):
         cleaned_content = self.cleaned_data.get('content')
         
         return cleaned_content
+    
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        label='',
+        widget=forms.TextInput(
+            attrs={'class': 'auth-input', 'placeholder': 'Username'}
+        )
+    )
+    email = forms.CharField(
+        max_length=255,
+        label='',
+        widget=forms.TextInput(
+            attrs={'class': 'auth-input', 'placeholder': 'Email'}
+        )
+    )
+    password1 = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(
+            attrs={'class': 'auth-input', 'placeholder': 'New Password'}
+        )
+    )
+    password2 = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(
+            attrs={'class': 'auth-input', 'placeholder': 'Confirm Password'}
+        )
+    )
+
+
+class LogInForm(forms.Form):
+    username = forms.CharField(
+        max_length=150,
+        label='',
+        widget=forms.TextInput(
+            attrs={'class': 'auth-input', 'placeholder': 'Username'}
+        )
+    )
+    password = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(
+            attrs={'class': 'auth-input', 'placeholder': 'Password'}
+        )
+    )
